163b5801faf8dce356aa0428bd427f7e817b01f8
Henos22 <108184340+Henos22@users.noreply.github.com>
2024-02-18 23:45:50 +0000

    Create a Downloads directory if it does not already exist.  Download the csv from each valid zip file address and write it as a csv to the Downloads directory.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,8 @@
 import requests
+from io import BytesIO
+import os 
+import pandas as pd
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -10,9 +14,47 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+def create_downloads_directory_if_not_exists() -> None:
+
+    directory = "Downloads"
+    parent_directory = "/Users/henostsegai/work/data-engineering-exercise/Exercises"
+
+    if os.path.isdir(f"{parent_directory}/{directory}"):
+
+        print("Downloads directory already exists")
+        return
+    
+    path = os.path.join(parent_directory, directory)
+    return os.mkdir(path)
+
+def download_csvs_from_zip_files():
+
+    for url in download_uris[:1]:
+
+        csv_file_name = f"{url.split('/')[-1][:-3]}csv"
+        print("csv file name: ",csv_file_name)
+
+        try:
+
+            filename = requests.get(url).content
+            zf = ZipFile( BytesIO(filename), 'r' )
+
+            for filename in zf.namelist():
+
+                print("File in zip: "+ filename)
+
+                if csv_file_name == filename:
+
+                    df = pd.read_csv( zf.open(filename), encoding="latin-1")
+                    df.to_csv(path_or_buf=f"/Users/henostsegai/work/data-engineering-exercise/Exercises/Downloads/{csv_file_name}")
+        except:
+
+            print(f"The following url is not valid: {url}")
+    return
 
 def main():
-    # your code here
+    create_downloads_directory_if_not_exists()
+    download_csvs_from_zip_files()
     pass
 
 
